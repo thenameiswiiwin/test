6830ae79de8d539de9214d2163ae5e9af73e86f9
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-08-18 10:33:01 -0500

    form event handling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,39 @@
+import { useState } from 'react'
 import Charizard from '../assets/charizard.jpg'
 
 function App() {
+  const [pokemonName, setPokemonName] = useState('')
+
+  const handleChange = (e) => setPokemonName(e.target.value)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setPokemonName(pokemonName)
+  }
+
+  const handleSelect = (pokemonName) => setPokemonName(pokemonName)
+
   return (
     <div className="flex flex-col justify-center items-center mt-14 font-semibold">
-      <form className="flex flex-col jutify-center items-center">
+      <form
+        className="flex flex-col jutify-center items-center"
+        onSubmit={handleSubmit}
+      >
         <label htmlFor="pokemonName-input">Pokemon Name</label>
         <small>
-          Try <button type="button">"pikachu"</button>
+          Try <button type="button" onClick={() => handleSelect('pikachu')}>"pikachu"</button>
           {', '}
-          <button type="button">"charizard"</button>
+          <button type="button" onClick={() => handleSelect('charizard')}>"charizard"</button>
           {', or '}
-          <button type="button">"mew"</button>
+          <button type="button" onClick={() => handleSelect('mew')}>"mew"</button>
         </small>
         <div>
           <input
             className="mt-2.5 mr-2.5 px-2.5 leading-loose rounded-sm box shadow-md bg-zinc-100"
             name="pokemonName"
             placeholder="Pokemon Name..."
+            value={pokemonName}
+            onChange={handleChange}
           />
           <button
             className="py-1.5 px-2.5 border border-solid rounded-md bg-red-600 text-white hover:bg-red-700 disabled:bg-red-700"
