a7fb1d684ffc619ca0a43e39d0186fe7d26c4f1f
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-08-18 15:48:29 -0500

    created PokemonInfo component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,42 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Charizard from '../assets/charizard.jpg'
+
+function PokemonInfo() {
+  return (
+    <div className="h-full flex flex-col items-center">
+      <section className="mb-4">
+        <small className="mb-1 float-right">10:08 50.405</small>
+        <img src={Charizard} alt="charizard" />
+        <h2 className="mt-2 text-center text-2xl font-extrabold">
+          Charizard
+          <sup>006</sup>
+        </h2>
+      </section>
+      <section>
+        <ul className="leading-none list-disc">
+          <li>
+            Dragon Claw: 35 <small>(Dragon)</small>
+          </li>
+          <li>
+            Fire Blast: 100 <small>(Fire)</small>
+          </li>
+          <li>
+            Flamethrower: 55 <small>(Fire)</small>
+          </li>
+        </ul>
+      </section>
+    </div>
+  )
+}
 
 function App() {
   const [pokemonName, setPokemonName] = useState('')
 
   const handleChange = (e) => setPokemonName(e.target.value)
-
   const handleSubmit = (e) => {
     e.preventDefault()
     setPokemonName(pokemonName)
   }
-
   const handleSelect = (pokemonName) => setPokemonName(pokemonName)
 
   return (
@@ -21,11 +47,18 @@ function App() {
       >
         <label htmlFor="pokemonName-input">Pokemon Name</label>
         <small>
-          Try <button type="button" onClick={() => handleSelect('pikachu')}>"pikachu"</button>
+          Try{' '}
+          <button type="button" onClick={() => handleSelect('pikachu')}>
+            "pikachu"
+          </button>
           {', '}
-          <button type="button" onClick={() => handleSelect('charizard')}>"charizard"</button>
+          <button type="button" onClick={() => handleSelect('charizard')}>
+            "charizard"
+          </button>
           {', or '}
-          <button type="button" onClick={() => handleSelect('mew')}>"mew"</button>
+          <button type="button" onClick={() => handleSelect('mew')}>
+            "mew"
+          </button>
         </small>
         <div>
           <input
@@ -38,6 +71,7 @@ function App() {
           <button
             className="py-1.5 px-2.5 border border-solid rounded-md bg-red-600 text-white hover:bg-red-700 disabled:bg-red-700"
             type="submit"
+            disabled={!pokemonName.length}
           >
             Submit
           </button>
@@ -47,29 +81,7 @@ function App() {
       <hr className="my-7" />
 
       <div className="h-96 w-72 bg-zinc-100 rounded overflow-auto p-5">
-        <div className="h-full flex flex-col items-center">
-          <section className="mb-4">
-            <small className="mb-1 float-right">10:08 50.405</small>
-            <img src={Charizard} alt="charizard" />
-            <h2 className="mt-2 text-center text-2xl font-extrabold">
-              charizard
-              <sup>006</sup>
-            </h2>
-          </section>
-          <section>
-            <ul className="leading-none list-disc">
-              <li>
-                Dragon Claw: 35 <small>(Dragon)</small>
-              </li>
-              <li>
-                Fire Blast: 100 <small>(Fire)</small>
-              </li>
-              <li>
-                Flamethrower: 55 <small>(Fire)</small>
-              </li>
-            </ul>
-          </section>
-        </div>
+        <PokemonInfo />
       </div>
     </div>
   )
