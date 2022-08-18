51f168ac1446f5844abbf8634bb29313bb05bddf
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-08-18 10:27:45 -0500

    added forum and card layout, with styling.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,57 @@
+import Charizard from '../assets/charizard.jpg'
+
 function App() {
   return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-screen-xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-        <div className="text-center">
-          <h2 className="text-base font-semibold uppercase tracking-wide text-blue-600">
-            Pokemon
-          </h2>
-          <p className="my-3 text-4xl font-bold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
-            This is a App about Pokemon Stats
-          </p>
-          <p className="text-xl text-gray-400">Let's Start building.</p>
+    <div className="flex flex-col justify-center items-center mt-14 font-semibold">
+      <form className="flex flex-col jutify-center items-center">
+        <label htmlFor="pokemonName-input">Pokemon Name</label>
+        <small>
+          Try <button type="button">"pikachu"</button>
+          {', '}
+          <button type="button">"charizard"</button>
+          {', or '}
+          <button type="button">"mew"</button>
+        </small>
+        <div>
+          <input
+            className="mt-2.5 mr-2.5 px-2.5 leading-loose rounded-sm box shadow-md bg-zinc-100"
+            name="pokemonName"
+            placeholder="Pokemon Name..."
+          />
+          <button
+            className="py-1.5 px-2.5 border border-solid rounded-md bg-red-600 text-white hover:bg-red-700 disabled:bg-red-700"
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+
+      <hr className="my-7" />
+
+      <div className="h-96 w-72 bg-zinc-100 rounded overflow-auto p-5">
+        <div className="h-full flex flex-col items-center">
+          <section className="mb-4">
+            <small className="mb-1 float-right">10:08 50.405</small>
+            <img src={Charizard} alt="charizard" />
+            <h2 className="mt-2 text-center text-2xl font-extrabold">
+              charizard
+              <sup>006</sup>
+            </h2>
+          </section>
+          <section>
+            <ul className="leading-none list-disc">
+              <li>
+                Dragon Claw: 35 <small>(Dragon)</small>
+              </li>
+              <li>
+                Fire Blast: 100 <small>(Fire)</small>
+              </li>
+              <li>
+                Flamethrower: 55 <small>(Fire)</small>
+              </li>
+            </ul>
+          </section>
         </div>
       </div>
     </div>
