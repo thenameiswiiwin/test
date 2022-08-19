c4f87e8e6cc730b506a21128de3f2235b9829c99
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-08-18 22:34:52 -0500

    created PokemonDataView component

---
+++ src/components/PokemonDataView.tsx
@@ -0,0 +1,32 @@
+function PokemonDataView({ pokemon }) {
+  return (
+    <div className="h-full flex flex-col items-center">
+      <section className="mb-4 flex flex-col justify-center">
+        <small className="mb-1 self-end">{pokemon.fetchedAt}</small>
+        <img
+          className="max-w-full max-h-52"
+          src={pokemon.image}
+          alt={pokemon.name}
+        />
+        <h2 className="mt-2 text-center text-2xl font-extrabold">
+          {pokemon.name}
+          <sup>{pokemon.number}</sup>
+        </h2>
+      </section>
+      <section>
+        <ul className="leading-none list-disc">
+          {pokemon.attacks.special.map((attack) => (
+            <li key={attack.name}>
+              <label>{attack.name}</label>:{' '}
+              <span>
+                {attack.damage} <small>({attack.type})</small>
+              </span>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  )
+}
+
+export default PokemonDataView
--- src/components/PokemonInfo.tsx
+++ src/components/PokemonInfo.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import PokemonDataView from './PokemonDataView'
 import fetchPokemon from '../utils/index'
 
 function PokemonInfo({ pokemonName }) {
@@ -13,34 +14,7 @@ function PokemonInfo({ pokemonName }) {
   if (!pokemonName) {
     return 'Submit a pokemon'
   } else if (pokemon) {
-    return (
-      <div className="h-full flex flex-col items-center">
-        <section className="mb-4 flex flex-col justify-center">
-          <small className="mb-1 self-end">{pokemon.fetchedAt}</small>
-          <img
-            className="max-w-full max-h-52"
-            src={pokemon.image}
-            alt={pokemon.name}
-          />
-          <h2 className="mt-2 text-center text-2xl font-extrabold">
-            {pokemon.name}
-            <sup>{pokemon.number}</sup>
-          </h2>
-        </section>
-        <section>
-          <ul className="leading-none list-disc">
-            {pokemon.attacks.special.map((attack) => (
-              <li key={attack.name}>
-                <label>{attack.name}</label>:{' '}
-                <span>
-                  {attack.damage} <small>({attack.type})</small>
-                </span>
-              </li>
-            ))}
-          </ul>
-        </section>
-      </div>
-    )
+    return <PokemonDataView pokemon={pokemon} />
   }
 }
 
